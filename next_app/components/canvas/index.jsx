721ba7b716b3516ed9bad60ca489e0339aacbbdd
Dhruvil4625 <dkrramolia20@gmail.com>
2026-03-22 16:17:08 +0530

    feat: Integrate XR and ARButton components into CanvasModel for enhanced AR support

--- next_app/components/canvas/index.jsx
+++ next_app/components/canvas/index.jsx
@@ -2,6 +2,7 @@
 import { Canvas } from '@react-three/fiber'
 import { Center, PresentationControls } from '@react-three/drei';
 import { Suspense } from 'react';
+import { XR, ARButton, useXR } from '@react-three/xr';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
@@ -16,27 +17,31 @@ const CanvasModel = () => {
       className="w-full max-w-full h-full transition-all ease-in"
       onCreated={({ scene }) => { window.appScene = scene }}
     >
-      <ambientLight intensity={1.5} />
-      <directionalLight position={[0, 0, 10]} intensity={2} />
+      <XR referenceSpace="local-floor">
+        <ARButton className="absolute z-20 top-5 left-5 px-3 py-2 rounded-md bg-black/80 text-white text-sm" />
 
-      <CameraRig>
-        <Backdrop />
-        <Center>
-          <PresentationControls 
-            global={true}
-            cursor={true}
-            snap={false}
-            speed={2}
-            zoom={1}
-            polar={[-Math.PI / 2, Math.PI / 2]}
-            azimuth={[-Infinity, Infinity]}
-          >
-            <Suspense fallback={null}>
-              <Shirt />
-            </Suspense>
-          </PresentationControls>
-        </Center>
-      </CameraRig>
+        <ambientLight intensity={1.5} />
+        <directionalLight position={[0, 0, 10]} intensity={2} />
+
+        <CameraRig>
+          <Backdrop />
+          <Center>
+            <PresentationControls 
+              global={true}
+              cursor={true}
+              snap={false}
+              speed={2}
+              zoom={1}
+              polar={[-Math.PI / 2, Math.PI / 2]}
+              azimuth={[-Infinity, Infinity]}
+            >
+              <Suspense fallback={null}>
+                <Shirt />
+              </Suspense>
+            </PresentationControls>
+          </Center>
+        </CameraRig>
+      </XR>
     </Canvas>
   )
 }
